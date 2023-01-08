1cbf004e99a50d03b84ed93fc7376db3c1f920f8
aamer Essa <aamer.es12@gmail.com>
2023-01-08 09:39:47 +0300

    Initial Commit

---
+++ Family Grocery - Belt Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Family Grocery - Belt Exam
+//
+//  Created by Aamer Essa on 08/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
